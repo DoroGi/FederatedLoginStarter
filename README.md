1facab99c2148e90b4a5502355162c503d0fa3ab
Gianmarco Doro <gianmarco.doro@gmail.com>
2020-07-15 11:25:58 +0200

    Add rudimental but complete SAML flow

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Federated login starter
 
 Small example of a Node JS server with federated login
+
+## How to run
+First you will need to configure your SAML Identity Provider.
+I used Azure Active Directory as I had it available, but I read that https://samltest.id/ is good too for testing.
+You will need to open "index.ts" and put the configuration for the endpoint in the Strategy configuration.
+
+Second, you will need to serve the page over https.
+This is not actually mandatory, but Azure does not let you set http endpoints.
+You will need to put the certificates in the `src` folder.
+This is a command to generate self-signed certificates:
+
+    openssl req -nodes -new -x509 -keyout server.key -out server.cert
+
+Finally, you need to run
+
+    yarn
+    yarn build
+    yarn start
+
+And it should work
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,89 @@
+import fs from 'fs'
+import https from 'https'
 import express from 'express'
-import { urlencoded } from 'body-parser'
+import session from 'express-session'
+import passport from 'passport'
+import { Strategy, Profile, VerifiedCallback } from 'passport-saml'
+import bodyParser from 'body-parser'
 
+//Certificate configuration
+const key = fs.readFileSync(__dirname + '/server.key')
+const cert = fs.readFileSync(__dirname + '/server.cert')
+
+//Passport.js configuration
+passport.serializeUser((user, done) => {
+    done(null, user)
+})
+
+passport.deserializeUser((user, done) => {
+    done(null, user)
+})
+
+passport.use(new Strategy({
+        path: '[SET HERE THE CALLBACK FOR THE SAML RESPONSE, FOR EXAMPLE: /callback]',
+        entryPoint: '[SET HERE THE SAML LOGIN URL]',
+        issuer: '[SET HERE THE APPLICATION IDENTIFIER]'
+    },
+    (profile: Profile, done: VerifiedCallback) => done(null, {
+        id: profile.uid,
+        email: profile.email,
+        displayName: profile.cn,
+        firstName: profile.givenName,
+        lastName: profile.sn
+    })
+))
+
+//Epress.js configuration
 const app = express()
-const port = 3000
+const port = 9090
+
+app.use(session(
+    {
+        resave: true,
+        saveUninitialized: true,
+        secret: 'The answer is 42'
+    }
+))
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.get('/healthcheck', (req, res) => res.sendStatus(200))
 
-app.get('/login', (req, res) => res.sendFile('page.html', {root: __dirname }))
-
-app.post('/auth', urlencoded({extended:false}), (req,res) => {
-    if (typeof req.body.username === 'string' && typeof req.body.password === 'string') {
-        //call identity provider
-        res.sendStatus(200)
+app.get('/', (req, res) => {
+    if (req.isAuthenticated()) {
+        res.sendFile('loggedpage.html', {root: __dirname })
     }
-    else res.sendStatus(400)
+    else {
+        res.sendFile('page.html', {root: __dirname })
+    }
+})
+app.get('/free', (req, res) => res.sendFile('free.html', {root: __dirname }))
+app.get('/protected', (req, res) => {
+    if(req.isAuthenticated()) {
+        res.sendFile('protected.html', {root: __dirname })
+    }
+    else {
+        res.redirect('/')
+    }
 })
 
-app.listen(port, () => { console.log(`Listening on port ${port}`)})
+app.get('/login',
+    passport.authenticate('saml', { failureRedirect: '/break1', successRedirect: '/success' })
+)
+app.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/')
+})
+
+app.post('/callback',
+    bodyParser.urlencoded({ extended: false }),
+    passport.authenticate('saml', { failureRedirect: '/break2' }),
+    (req, res) => {
+        res.redirect('/');
+    }
+)
+
+//Web server configuration
+const server = https.createServer({ key: key, cert: cert }, app)
+
+server.listen(port, () => { console.log(`Listening on port ${port}`)})
